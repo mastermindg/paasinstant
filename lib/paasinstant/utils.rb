6cf78fbb4c6d4b8be1562b3f27ca0bce0d0860d2
Ken Jenney <mastermindg@gmail.com>
2016-08-25 11:16:28 -0400

    Updating

--- lib/paasinstant/utils.rb
+++ lib/paasinstant/utils.rb
@@ -20,13 +20,21 @@ module Paasinstant
 
 		def ssh_run(hostname, cmd)
 			username = "root"
+			alert = "connecting to #{hostname} using #{username}" 
 			begin
-    			ssh = Net::SSH.start(hostname, username)
-    			res = ssh.exec!(cmd)
-    			ssh.close
-    			puts res
-  			rescue
-    			puts "Unable to connect to #{@hostname} using #{@username}}"
+    			Net::SSH.start(hostname, username, :timeout => 4) do |ssh|
+    				res = ssh.exec!(cmd)
+    				ssh.close
+    				puts res
+    			end
+  			rescue Net::SSH::ConnectionTimeout
+    			puts "\tTimed out #{alert}"
+  			rescue Errno::EHOSTUNREACH
+    			puts "\tHost unreachable #{alert}"
+  			rescue Errno::ECONNREFUSED
+    			puts "\tConnection refused #{alert}"
+  			rescue Net::SSH::AuthenticationFailed
+    			puts "\tAuthentication failure #{alert}"
   			end
 		end
 	end
